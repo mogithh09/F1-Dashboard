4a9c8a4cab2eacff932f85ab4b82c34d7290855f
mogithh09 <mogithh09@gmail.com>
2026-04-09 20:22:44 +0530

    F1 summary dashboard

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as py
+import numpy as np
+import seaborn as sns 
+import streamlit as st 
+
+##title##
+st.images("https://1000logos.net/f1-logo/",width=200)
+st.title("F1 Summary Dasboard")
+st.write("Race summary")
