211e5e19a0a643b9464e78e5894d3491e0ab71c9
mogithh09 <mogithh09@gmail.com>
2026-04-09 22:02:30 +0530

    Remove sidebar navigation from app.py
    Removed sidebar navigation and related code.

--- app.py
+++ app.py
@@ -7,16 +7,5 @@ import streamlit as st
 st.image("https://logos-world.net/wp-content/uploads/2023/12/F1-Logo.png",width=200)
 st.title("F1 Summary Dasboard")
 
-##sidebar##
-st.sidebar.write("Navigation")
-page=st.sidebar.selectbox("Select option :",["Race summary","Race Stratergy"] )
-if page =="Race summary":
-  st.header("Race Summary")
-else:
-  st.header("Race stratergy")
 
-import fastf1
-year=2026
-schedule=fastf1.get_event_schedule(year)
-st.selectbox("2026 Races :",{schedule})
 
