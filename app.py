37c57e237ccba6d418a74915cbde314cdd0a436b
mogithh09 <mogithh09@gmail.com>
2026-04-09 20:49:37 +0530

    Update F1 logo image URL in app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import seaborn as sns
 import streamlit as st 
 
 ##title##
-st.image("https://www.pngfind.com/mpng/ixbwbob_file-f1-svg-f1-logo-png-transparent-png/",width=200)
+st.image("https://wallpaperaccess.com/f1-logo",width=200)
 st.title("F1 Summary Dasboard")
 st.write("Race summary")
