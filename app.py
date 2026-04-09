4424c949023b3880b700993db86baecf006a9073
mogithh09 <mogithh09@gmail.com>
2026-04-09 20:46:09 +0530

    Update F1 logo image URL in app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import seaborn as sns
 import streamlit as st 
 
 ##title##
-st.image("https://wallpaperaccess.com/download/f1-logo-2180256",width=200)
+st.image("https://medium.com/territory-projects/racing-ahead-the-new-f1-logo-looks-to-the-future-fcd1aecf9504",width=200)
 st.title("F1 Summary Dasboard")
 st.write("Race summary")
