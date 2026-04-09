22efbfb07213348afbf63cf314d01f2408eca7d4
mogithh09 <mogithh09@gmail.com>
2026-04-09 20:47:40 +0530

    Update image source in app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import seaborn as sns
 import streamlit as st 
 
 ##title##
-st.image("https://medium.com/territory-projects/racing-ahead-the-new-f1-logo-looks-to-the-future-fcd1aecf9504",width=200)
+st.image("https://in.pinterest.com/pin/71353975342187682/",width=200)
 st.title("F1 Summary Dasboard")
 st.write("Race summary")
