6f39d3948500bb1b5258c05525a1ae454d666da5
mogithh09 <mogithh09@gmail.com>
2026-04-09 20:48:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ import seaborn as sns
 import streamlit as st 
 
 ##title##
-st.image("https://in.pinterest.com/pin/71353975342187682/",width=200)
+st.image("https://www.pngfind.com/mpng/ixbwbob_file-f1-svg-f1-logo-png-transparent-png/",width=200)
 st.title("F1 Summary Dasboard")
 st.write("Race summary")
