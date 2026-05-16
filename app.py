99c81dc585de21776002c6b18db6a40868b84d10
mogithh09 <mogithh09@gmail.com>
2026-05-16 14:05:36 +0530

    Display circuit_id in the first column

--- app.py
+++ app.py
@@ -213,6 +213,7 @@ if info:
     col1, col2 = st.columns(2)
 
     with col1:
+        st.write(circuit_id)
         image_path = f"{circuit_id}.webp"
         st.image(image_path, use_container_width=True)
 
