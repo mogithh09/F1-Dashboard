823d9b124621352012f5626c5b083372eee8dbb8
mogithh09 <mogithh09@gmail.com>
2026-05-19 20:11:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -278,4 +278,5 @@ else:
 
 time=results[0]['FastestLap']['Time']['time']
 st.header('🟣FASTEST LAP')
-st.write(results[0]['Driver'],time)
+driver=results[0]['FastsstLap']['Driver']['FamilyName']
+st.write(driver,time)
